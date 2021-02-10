ab1728fc29e0d2f4ef371f255af72ede02086f14
Kne Rock <kne.rock@gmail.com>
2021-02-10 16:18:52 +0530

    Updated Commit

--- app.py
+++ app.py
@@ -3,3 +3,5 @@ import streamlit as st
 st.title("STREMLIT HEROKU!")
 st.subheader("Welcome to my stremlit app")
 st.text("Some text here")
+
+st.subheader("It is updated application deploying again")
